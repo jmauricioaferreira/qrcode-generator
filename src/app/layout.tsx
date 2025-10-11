3b38be57ef6aa18020a6150452c95fef21e918e6
jmauricio <jmauricio.a.ferreira@gmail.com>
2025-10-11 16:35:56 -0300

    Remove Google AdSense script from layout.tsx to streamline code and improve performance

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -64,12 +64,6 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <head>
-        <Script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1113577441435329"
-          crossOrigin="anonymous"
-          strategy="afterInteractive"
-        />
         {/* High Performance Format Script */}
         <Script
           id="hpf-config"
