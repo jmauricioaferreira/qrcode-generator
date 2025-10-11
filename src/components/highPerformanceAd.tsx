31eea5b34cbeb776d3547a41a536960ec55bd4db
jmauricio <jmauricio.a.ferreira@gmail.com>
2025-10-11 17:39:38 -0300

    Refactor HighPerformanceAd component to improve ad loading mechanism
    - Replaced direct DOM manipulation with a global window object for ad options.
    - Utilized Next.js Script component for loading the ad script after the page becomes interactive.
    - Simplified the component structure by removing unnecessary refs and loading messages.

--- src/components/highPerformanceAd.tsx
+++ src/components/highPerformanceAd.tsx
@@ -1,36 +1,32 @@
 // src/components/highPerformanceAd.tsx
 "use client";
-import { useEffect, useRef } from "react";
+import Script from "next/script";
+import { useEffect } from "react";
 
 export default function HighPerformanceAd() {
-  const banner = useRef<HTMLDivElement>(null);
-
   useEffect(() => {
-    if (banner.current && !banner.current.firstChild) {
-      const atOptions = {
-        key: "fa9730f7de22053acc5e430b64a5ba84",
-        format: "iframe",
-        height: 250,
-        width: 300,
-        params: {},
-      };
-      const conf = document.createElement("script");
-      const script = document.createElement("script");
-      script.type = "text/javascript";
-      script.src = `//www.highperformanceformat.com/fa9730f7de22053acc5e430b64a5ba84/invoke.js`;
-      conf.innerHTML = `atOptions = ${JSON.stringify(atOptions)}`;
-
-      banner.current.append(conf);
-      banner.current.append(script);
-    }
-  }, [banner]);
+    window.atOptions = {
+      key: "fa9730f7de22053acc5e430b64a5ba84",
+      format: "iframe",
+      height: 250,
+      width: 300,
+      params: {},
+    };
+  }, []);
 
   return (
     <div
       className="w-full max-w-[300px] h-[250px] mx-2 my-5 text-center flex flex-col justify-center items-center border border-gray-200 bg-gray-50 rounded-lg"
-      ref={banner}
+      style={{ display: "flex", justifyContent: "center", margin: "20px 0" }}
     >
-      <div className="text-gray-500 text-sm">Carregando anúncio...</div>
+      <Script
+        id="adsterra-script"
+        src="//www.highperformanceformat.com/fa9730f7de22053acc5e430b64a5ba84/invoke.js"
+        strategy="afterInteractive" // Carrega o script após a página se tornar interativa
+        onError={(e) => {
+          console.error("Script da Adsterra falhou ao carregar", e);
+        }}
+      />
     </div>
   );
 }
